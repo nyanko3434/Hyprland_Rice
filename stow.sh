eec35fd1a040565987e33781ba892ccaaccbeb78
nyanko3434 <khusunayan@gmail.com>
2025-11-28 14:43:19 +0545

    Add default keyd configuration and update stow script for symlink

--- stow.sh
+++ stow.sh
@@ -1,2 +1,2 @@
 stow -d ~/Documents/Projects/Hyprland_Rice/ -t ~/ dotfiles
-sudo ln -s ~/Documents/Projects/Hyprland_Rice/dotfiles/etc/keyd/default.conf /etc/keyd/default.conf
+sudo ln -s ~/Documents/Projects/Hyprland_Rice/etc/keyd/default.conf /etc/keyd/default.conf
