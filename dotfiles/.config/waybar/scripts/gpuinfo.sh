6bc64def3f810ffaea81d5dc1c04c8355cf55fe9
nyanko3434 <khusunayan@gmail.com>
2025-12-02 11:41:30 +0545

    Refactor GPU info script to include temperature data and clean up module configurations

--- dotfiles/.config/waybar/scripts/gpuinfo.sh
+++ dotfiles/.config/waybar/scripts/gpuinfo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 GPU_UTIL=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
-echo "{\"percentage\": $GPU_UTIL, \"tooltip\": \"GPU Utilization: $GPU_UTIL%\"}"
+GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
+echo "{\"percentage\": $GPU_UTIL,\"tooltip\": \"GPU Utilization: $GPU_UTIL%\nGPU Temperature: $GPU_TEMP°C\"}"
 
 	
