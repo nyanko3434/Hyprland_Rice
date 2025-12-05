aa37b0bbbeb4ccfe443063aac6269bae6ab5ef20
nyanko3434 <khusunayan@gmail.com>
2025-12-06 01:11:19 +0545

    Refactor hardware module configuration and simplify toggle script

--- dotfiles/.config/waybar/scripts/toggle_hardware.sh
+++ dotfiles/.config/waybar/scripts/toggle_hardware.sh
@@ -4,8 +4,6 @@ CONFIG="$HOME/.config/waybar/config"
 
 hw_start="@hardware_start"
 hw_end="@hardware_end"
-co_start="@collapse_start"
-co_end="@collapse_end"
 
 # Check if hardware block is active (uncommented)
 hw_active=$(sed -n "/$hw_start/,/$hw_end/p" "$CONFIG" | grep -v "^//")
@@ -13,11 +11,9 @@ hw_active=$(sed -n "/$hw_start/,/$hw_end/p" "$CONFIG" | grep -v "^//")
 if [ -n "$hw_active" ]; then
     # Hardware group active → collapse it
     sed -i "/$hw_start/,/$hw_end/s/^/\/\//" "$CONFIG"
-    sed -i "/$co_start/,/$co_end/s/^\/\///" "$CONFIG"
 else
     # Collapsed module active → expand hardware
     sed -i "/$hw_start/,/$hw_end/s/^\/\///" "$CONFIG"
-    sed -i "/$co_start/,/$co_end/s/^/\/\//" "$CONFIG"
 fi
 
 # Reload Waybar
